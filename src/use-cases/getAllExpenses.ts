87e651a2b7a7c768c9622c36f6438bba4ed9310d
Caalb <carlosmunizfjunior@gmail.com>
2024-11-04 20:06:27 -0300

    feat: implement GetAllExpenses use case for retrieving all expenses

---
+++ src/use-cases/getAllExpenses.ts
@@ -0,0 +1,10 @@
+import { Expenses } from "../domain/entities/Expenses";
+import { ExpensesRepository } from "../domain/interfaces/ExpensesRepository";
+
+export class GetAllExpenses {
+  constructor(private readonly expensesRepository: ExpensesRepository) {}
+
+  async execute(): Promise<Expenses[]> {
+    return await this.expensesRepository.getAll();
+  }
+}
