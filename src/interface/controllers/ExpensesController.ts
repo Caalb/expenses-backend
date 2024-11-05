41dd65393f58d299fe66f4f33617f9d4c07ff2f4
Caalb <carlosmunizfjunior@gmail.com>
2024-11-04 23:53:32 -0300

    feat: add update method to ExpensesController

--- src/interface/controllers/ExpensesController.ts
+++ src/interface/controllers/ExpensesController.ts
@@ -8,6 +8,7 @@ export class ExpensesController {
   private getAllExpenses = DIContainer.getGetAllExpensesUseCase();
   private createExpenses = DIContainer.getCreateExpensesUseCase();
   private deleteExpenses = DIContainer.getDeleteExpensesUseCase();
+  private updateExpenses = DIContainer.getUpdateExpensesUseCase();
 
   async getAll(req: Request, res: Response): Promise<void> {
     const expenses = await this.getAllExpenses.execute();
@@ -24,6 +25,19 @@ export class ExpensesController {
 
     const expense = await this.createExpenses.execute(dto);
     return res.status(201).json(expense);
+  }
+
+  async update(req: Request, res: Response): Promise<any> {
+    const expenseId = req.params.id;
+    const dto = Object.assign(new CreateExpensesDto(), req.body);
+    const errors = await validate(dto);
+
+    if (errors.length > 0) {
+      return res.status(400).json({ errors });
+    }
+
+    const expense = await this.updateExpenses.execute(expenseId, dto);
+    return res.status(200).json(expense);
   }
 
   async delete(req: Request, res: Response): Promise<void> {
