0136aaac1e446815f9ce25e853173fef05d3b72c
Caalb <carlosmunizfjunior@gmail.com>
2024-11-05 18:36:10 -0300

    feat: remove unused user-related dependencies from DIExpensesContainer

--- src/infrastructure/DIExpensesContainer.ts
+++ src/infrastructure/DIExpensesContainer.ts
@@ -3,13 +3,9 @@ import { GetAllExpenses } from "../use-cases/expenses/getAllExpenses";
 import { DeleteExpense } from "../use-cases/expenses/deleteExpense";
 import { UpdateExpenses } from "../use-cases/expenses/updateExpense";
 import { ExpensesRepository } from "./repositories/ExpensesRepository";
-import { UsersRepository } from "./repositories/UsersRepository";
-import { SignUpUser } from "../use-cases/auth/signUpUser";
-import { SignInUser } from "../use-cases/auth/signInUser";
 
 class DIExpensesContainer {
   private static _expensesRepository = new ExpensesRepository();
-  private static _usersRepository = new UsersRepository();
 
   static getExpensesRepository(): ExpensesRepository {
     return this._expensesRepository;
@@ -29,18 +25,6 @@ class DIExpensesContainer {
 
   static getUpdateExpensesUseCase(): UpdateExpenses {
     return new UpdateExpenses(DIExpensesContainer.getExpensesRepository());
-  }
-
-  static getUsersRepository(): UsersRepository {
-    return this._usersRepository;
-  }
-
-  static getSignUpUserUseCase(): SignUpUser {
-    return new SignUpUser(DIExpensesContainer.getUsersRepository());
-  }
-
-  static getSignInUserUseCase(): SignInUser {
-    return new SignInUser(DIExpensesContainer.getUsersRepository());
   }
 }
 
