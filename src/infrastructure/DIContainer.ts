3b99831bb7e3605231431182c1bf1cc8269741d0
Caalb <carlosmunizfjunior@gmail.com>
2024-11-05 17:41:24 -0300

    feat: add UsersRepository and SignUpUser use case to DIContainer

--- src/infrastructure/DIContainer.ts
+++ src/infrastructure/DIContainer.ts
@@ -3,9 +3,12 @@ import { GetAllExpenses } from "../use-cases/expenses/getAllExpenses";
 import { DeleteExpense } from "../use-cases/expenses/deleteExpense";
 import { UpdateExpenses } from "../use-cases/expenses/updateExpense";
 import { ExpensesRepository } from "./repositories/ExpensesRepository";
+import { UsersRepository } from "./repositories/UsersRepository";
+import { SignUpUser } from "../use-cases/auth/signUpUser";
 
 class DIContainer {
   private static _expensesRepository = new ExpensesRepository();
+  private static _usersRepository = new UsersRepository();
 
   static getExpensesRepository(): ExpensesRepository {
     return this._expensesRepository;
@@ -25,6 +28,14 @@ class DIContainer {
 
   static getUpdateExpensesUseCase(): UpdateExpenses {
     return new UpdateExpenses(DIContainer.getExpensesRepository());
+  }
+
+  static getUsersRepository(): UsersRepository {
+    return this._usersRepository;
+  }
+
+  static getSignUpUserUseCase(): SignUpUser {
+    return new SignUpUser(DIContainer.getUsersRepository());
   }
 }
 
