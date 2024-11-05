6d1713aaef21392c1143ba9dd335cc1bb80bd87c
Caalb <carlosmunizfjunior@gmail.com>
2024-11-04 21:27:14 -0300

    feat: add Swagger setup for API documentation

---
+++ src/interface/swagger.ts
@@ -0,0 +1,24 @@
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import { Express } from "express";
+import path from "path";
+
+console.log(path.join(__dirname, "routes/*.ts"));
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Extenses Control",
+      version: "1.0.0",
+    },
+  },
+  apis: [path.join(__dirname, "routes/*.ts")],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+function setupSwagger(app: Express) {
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+}
+
+export { setupSwagger };
