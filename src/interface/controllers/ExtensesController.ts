cdb834f34983f561ab1609860974f6014e9218cb
Caalb <carlosmunizfjunior@gmail.com>
2024-11-04 21:27:24 -0300

    feat: add create method to ExtensesController with validation

--- src/interface/controllers/ExtensesController.ts
+++ src/interface/controllers/ExtensesController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import { DIContainer } from "../../infrastructure/DIContainer";
+import { CreateBookDto } from "../dto/CreateExtensesDto";
+import { validate } from "class-validator";
 
 export class ExtensesController {
   private getAllExpenses = DIContainer.getGetAllExpensesUseCase();
@@ -7,5 +9,17 @@ export class ExtensesController {
   async getAll(req: Request, res: Response): Promise<void> {
     const expenses = await this.getAllExpenses.execute();
     res.status(200).json(expenses);
+  }
+
+  async create(req: Request, res: Response): Promise<any> {
+    const dto = Object.assign(new CreateBookDto(), req.body);
+    const errors = await validate(dto);
+
+    if (errors.length > 0) {
+      return res.status(400).json({ errors });
+    }
+
+    const expense = await this.getAllExpenses.execute();
+    return res.status(201).json(expense);
   }
 }
