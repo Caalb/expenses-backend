4eed90a25e8505cc906f89da040f06feb2750378
Caalb <carlosmunizfjunior@gmail.com>
2024-11-04 23:09:19 -0300

    refactor: update ExpensesController to use CreateExpensesDto and integrate createExpenses use case

--- src/interface/controllers/ExpensesController.ts
+++ src/interface/controllers/ExpensesController.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from "express";
 import { DIContainer } from "../../infrastructure/DIContainer";
-import { CreateBookDto } from "../dto/CreateExpensesDto";
+import { CreateExpensesDto } from "../dto/CreateExpensesDto";
 import { validate } from "class-validator";
 
 export class ExpensesController {
   private getAllExpenses = DIContainer.getGetAllExpensesUseCase();
+  private createExpenses = DIContainer.getCreateExpensesUseCase();
 
   async getAll(req: Request, res: Response): Promise<void> {
     const expenses = await this.getAllExpenses.execute();
@@ -12,14 +13,14 @@ export class ExpensesController {
   }
 
   async create(req: Request, res: Response): Promise<any> {
-    const dto = Object.assign(new CreateBookDto(), req.body);
+    const dto = Object.assign(new CreateExpensesDto(), req.body);
     const errors = await validate(dto);
 
     if (errors.length > 0) {
       return res.status(400).json({ errors });
     }
 
-    const expense = await this.getAllExpenses.execute();
+    const expense = await this.createExpenses.execute(dto);
     return res.status(201).json(expense);
   }
 }
