a064633e44bd3fe41b1cf3b74c81089936a803f5
Caalb <carlosmunizfjunior@gmail.com>
2024-11-04 23:09:01 -0300

    feat: implement ExpensesRepository for managing expenses with TypeORM

---
+++ src/infrastructure/repositories/ExpensesRepository.ts
@@ -0,0 +1,31 @@
+import { ExpensesEntity } from "../../infrastructure/database/entities/ExpensesEntity";
+import { Repository } from "typeorm";
+import { AppDataSource } from "../database/data-source";
+
+export class ExpensesRepository {
+  private repository: Repository<ExpensesEntity>;
+
+  constructor() {
+    this.repository = AppDataSource.getRepository(ExpensesEntity);
+  }
+
+  async create(expenses: ExpensesEntity): Promise<void> {
+    await this.repository.save(expenses);
+  }
+
+  async getAll(): Promise<ExpensesEntity[]> {
+    return await this.repository.find();
+  }
+
+  async getById(id: string): Promise<ExpensesEntity | null> {
+    return await this.repository.findOneBy({ id });
+  }
+
+  async update(expenses: ExpensesEntity): Promise<void> {
+    await this.repository.save(expenses);
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.repository.delete(id);
+  }
+}
