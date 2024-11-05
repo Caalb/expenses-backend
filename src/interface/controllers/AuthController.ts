8720e66a844598598d011b39e77f9080206550e5
Caalb <carlosmunizfjunior@gmail.com>
2024-11-05 18:22:17 -0300

    feat: implement signin method in AuthController with error handling

--- src/interface/controllers/AuthController.ts
+++ src/interface/controllers/AuthController.ts
@@ -3,10 +3,11 @@ import { Request, Response } from "express";
 import { DIContainer } from "../../infrastructure/DIContainer";
 import { SignUpUserDto } from "../dto/SignUpUserDto";
 import { SignInUserDto } from "../dto/SignInUserDto";
+import { AppError } from "../../shared/errors/AppError";
 
 export class AuthController {
   private signUpUser = DIContainer.getSignUpUserUseCase();
-  // private signInUser = DIContainer.getSignInUserUseCase();
+  private signInUser = DIContainer.getSignInUserUseCase();
 
   async signUp(req: Request, res: Response): Promise<any> {
     const dto = Object.assign(new SignUpUserDto(), req.body);
@@ -20,15 +21,22 @@ export class AuthController {
     return res.status(201).json(user);
   }
 
-  // async signin(req: Request, res: Response): Promise<any> {
-  //   const dto = Object.assign(new SignInUserDto(), req.body);
-  //   const errors = await validate(dto);
+  async signin(req: Request, res: Response): Promise<any> {
+    const dto = Object.assign(new SignInUserDto(), req.body);
+    const errors = await validate(dto);
 
-  //   if (errors.length > 0) {
-  //     return res.status(400).json({ errors });
-  //   }
+    if (errors.length > 0) {
+      return res.status(400).json({ errors });
+    }
 
-  //   const token = await this.signInUser.execute(dto);
-  //   return res.status(200).json({ token });
-  // }
+    try {
+      const token = await this.signInUser.execute(dto);
+      return res.status(200).json({ token });
+    } catch (error) {
+      if (error instanceof AppError) {
+        return res.status(error.statusCode).json({ message: error.message });
+      }
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+  }
 }
