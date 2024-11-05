58a1fb98164a9d52aa814d7d6c963435cca88fb1
Caalb <carlosmunizfjunior@gmail.com>
2024-11-04 23:08:34 -0300

    refactor: update Expenses entity to use 'amount' instead of 'value' and remove 'id' and 'userId' properties

--- src/domain/entities/Expenses.ts
+++ src/domain/entities/Expenses.ts
@@ -1,9 +1,7 @@
 export class Expenses {
   constructor(
-    public readonly id: string,
     public description: string,
     public date: Date,
-    public userId: string,
-    public value: number
+    public amount: number
   ) {}
 }
