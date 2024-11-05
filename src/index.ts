899bee0c4fbef4b11f51b724f337788f79600b60
Caalb <carlosmunizfjunior@gmail.com>
2024-11-04 22:12:17 -0300

    feat: initialize database connection and update routes to use expensesRoutes

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
+import "reflect-metadata";
 import express from "express";
-import { extensesRoutes } from "./interface/routes/extensesRoutes";
+import { expensesRoutes } from "./interface/routes/expensesRoutes";
 import { errorHandler } from "./interface/middleware/errorHandler";
 import { logger } from "./infrastructure/logger";
 import { setupSwagger } from "./interface/swagger";
+import { AppDataSource } from "./infrastructure/database/data-source";
 
 const app = express();
 
+AppDataSource.initialize()
+  .then(() => {
+    logger.info("Database connected");
+  })
+  .catch((error) => {
+    logger.error("Error connecting to database:", error);
+  });
+
 app.use(express.json());
-app.use("/", extensesRoutes);
+app.use("/", expensesRoutes);
 app.get("/ping", (req, res) => {
   res.send("pong");
 });
