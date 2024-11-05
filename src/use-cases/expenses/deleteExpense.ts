65e89e90c3b323e4591c1e62b87883d8a8bf9a60
Caalb <carlosmunizfjunior@gmail.com>
2024-11-05 18:33:41 -0300

    feat: update DeleteExpense use case to throw AppError for non-existent expense

--- src/use-cases/expenses/deleteExpense.ts
+++ src/use-cases/expenses/deleteExpense.ts
@@ -1,14 +1,16 @@
 import { Expenses } from "../../domain/entities/Expenses";
 import { ExpensesRepository } from "../../domain/interfaces/ExpensesRepository";
+import { AppError } from "../../shared/errors/AppError";
+import { HttpStatus } from "../../shared/httpStatus";
 
 export class DeleteExpense {
   constructor(private readonly expensesRepository: ExpensesRepository) {}
 
-  async execute(id: string): Promise<Expenses> {
+  async execute(id: string): Promise<Expenses | AppError> {
     const expense = await this.expensesRepository.getById(id);
 
     if (!expense) {
-      throw new Error("Expense not found");
+      throw new AppError("Despesa não encontrada", HttpStatus.NOT_FOUND);
     }
 
     await this.expensesRepository.delete(expense.id);
