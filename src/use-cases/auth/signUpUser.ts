d1b90c0d9e1acda0f2debdfa363930172e47d1ef
Caalb <carlosmunizfjunior@gmail.com>
2024-11-05 18:22:37 -0300

    feat: implement password hashing in SignUpUser use case

--- src/use-cases/auth/signUpUser.ts
+++ src/use-cases/auth/signUpUser.ts
@@ -3,21 +3,25 @@ import { v4 as uuidv4 } from "uuid";
 import { User } from "../../domain/entities/Users";
 import { UsersRepository } from "../../domain/interfaces/UsersRepository";
 import { SignUpUserDto } from "../../interface/dto/SignUpUserDto";
+import bcrypt from "bcrypt";
 
 export class SignUpUser {
   constructor(private readonly usersRepository: UsersRepository) {}
 
-  async execute(dto: SignUpUserDto, res: Response): Promise<User> {
+  async execute(dto: SignUpUserDto, res: Response): Promise<User | void> {
     const existingUser = await this.usersRepository.findByEmail(dto.email);
 
     if (existingUser) {
       res.status(422).json({ message: "Email já está em uso" });
+
+      return;
     }
 
+    const hashPassword = await bcrypt.hash(dto.password, 12);
     const user = new User({
       id: uuidv4(),
       email: dto.email,
-      password: dto.password,
+      password: hashPassword,
       name: dto.name,
     });
 
