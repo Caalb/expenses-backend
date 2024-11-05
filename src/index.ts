c0ca05333faec06e3ad32b64b594e065a62ce476
Caalb <carlosmunizfjunior@gmail.com>
2024-11-04 21:27:19 -0300

    feat: integrate Swagger setup and logger in the main application

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
 import express from "express";
 import { extensesRoutes } from "./interface/routes/extensesRoutes";
 import { errorHandler } from "./interface/middleware/errorHandler";
+import { logger } from "./infrastructure/logger";
+import { setupSwagger } from "./interface/swagger";
 
 const app = express();
 
 app.use(express.json());
 app.use("/", extensesRoutes);
-app.use(errorHandler);
 app.get("/ping", (req, res) => {
   res.send("pong");
 });
 
+setupSwagger(app);
+app.use(errorHandler);
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  logger.info(`Server is running on port ${3000}`);
 });
