3d26af2bfb992c54fda651e107b2aceea1fc9f17
Caalb <carlosmunizfjunior@gmail.com>
2024-11-05 21:23:23 -0300

    feat: modificar método getAll para aceitar userId no repositório de despesas

--- src/domain/interfaces/ExpensesRepository.ts
+++ src/domain/interfaces/ExpensesRepository.ts
@@ -2,7 +2,7 @@ import { Expenses } from "../entities/Expenses";
 
 export interface ExpensesRepository {
   create(expenses: Expenses): Promise<void>;
-  getAll(): Promise<Expenses[]>;
+  getAll(userId: string): Promise<Expenses[]>;
   getById(id: string): Promise<Expenses | null>;
   update(expenses: Expenses): Promise<void>;
   delete(id: string): Promise<void>;
