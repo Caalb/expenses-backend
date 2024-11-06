7f50a3dbe0c792fcee8b7ea513c870880ff5aa1d
Caalb <carlosmunizfjunior@gmail.com>
2024-11-05 21:23:43 -0300

    feat: modificar método execute para incluir userId ao criar despesas

--- src/use-cases/expenses/createExpense.ts
+++ src/use-cases/expenses/createExpense.ts
@@ -6,12 +6,15 @@ import { v4 as uuidv4 } from "uuid";
 export class CreateExpenses {
   constructor(private readonly expensesRepository: ExpensesRepository) {}
 
-  async execute(dto: CreateExpensesDto): Promise<Expenses> {
+  async execute(dto: CreateExpensesDto, userId: string): Promise<Expenses> {
     const expenses = new Expenses(
       uuidv4(),
       dto.description,
       dto.date,
-      dto.amount
+      dto.amount,
+      {
+        id: userId,
+      }
     );
 
     await this.expensesRepository.create(expenses);
