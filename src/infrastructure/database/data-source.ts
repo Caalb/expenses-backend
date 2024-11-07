215d992ff5639f8aefa5ea53552c0f1b72bb09d2
Caalb <carlosmunizfjunior@gmail.com>
2024-11-07 10:26:47 -0300

    refactor: adicionar URL do banco de dados no arquivo de configuração do DataSource

--- src/infrastructure/database/data-source.ts
+++ src/infrastructure/database/data-source.ts
@@ -4,6 +4,7 @@ import { UsersEntity } from "./entities/UsersEntity";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
+  url: process.env.POSTGRES_URL,
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
