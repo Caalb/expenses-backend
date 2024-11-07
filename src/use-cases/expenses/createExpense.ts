3439160db79532f472ce7bbda37542aaaa470e0b
Caalb <carlosmunizfjunior@gmail.com>
2024-11-07 00:27:31 -0300

    feat: integrar envio de email ao criar despesas, utilizando dados do usuário autenticado

--- src/use-cases/expenses/createExpense.ts
+++ src/use-cases/expenses/createExpense.ts
@@ -2,22 +2,40 @@ import { Expenses } from "../../domain/entities/Expenses";
 import { ExpensesRepository } from "../../domain/interfaces/ExpensesRepository";
 import { CreateExpensesDto } from "../../interface/dto/CreateExpensesDto";
 import { v4 as uuidv4 } from "uuid";
+import { EmailUseCases } from "../email/sendCreatedExpensesEmail";
+import { IUserJwtPayload } from "../../shared/types/UserJwtPayload";
+import { formatDate } from "../../shared/helpers/date";
 
 export class CreateExpenses {
-  constructor(private readonly expensesRepository: ExpensesRepository) {}
+  private emailUseCases: EmailUseCases;
 
-  async execute(dto: CreateExpensesDto, userId: string): Promise<Expenses> {
+  constructor(private readonly expensesRepository: ExpensesRepository) {
+    this.emailUseCases = new EmailUseCases();
+  }
+
+  async execute(
+    dto: CreateExpensesDto,
+    user: IUserJwtPayload
+  ): Promise<Expenses> {
     const expenses = new Expenses(
       uuidv4(),
       dto.description,
       dto.date,
       dto.amount,
       {
-        id: userId,
+        id: user.id,
       }
     );
 
     await this.expensesRepository.create(expenses);
+
+    await this.emailUseCases.sendCreatedExpensesEmail({
+      recipient: user.email,
+      name: user.name,
+      amount: dto.amount,
+      date: formatDate(String(dto.date)),
+      description: dto.description,
+    });
 
     return expenses;
   }
